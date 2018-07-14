0ea098a36e4863e3757913680a814e721ab368ce
Wilk Bartlomiej <wilkbartlomiej26@gmail.com>
2018-07-14 16:08:59 +0200

    Player + Script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 1.8f;
+    public float horizontalLimit = 2.6f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Horizontal Movement
+        GetComponent<Rigidbody2D>().velocity = new Vector2(
+            Input.GetAxis("Horizontal") * speed,
+            0
+            );
+
+        //Keep the player inside the screen
+
+        if (transform.position.x > horizontalLimit)
+        {
+            transform.position = new Vector3(horizontalLimit, transform.position.y, transform.position.z);
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        } if (transform.position.x < -horizontalLimit)
+        {
+            transform.position = new Vector3(-horizontalLimit, transform.position.y, transform.position.z);
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
+    }
+}
