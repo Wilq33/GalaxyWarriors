0b55d6f98b4b69b917f9b75d727934db55997a98
Wilk Bartlomiej <wilkbartlomiej26@gmail.com>
2018-07-14 19:45:06 +0200

    Firing logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     public float speed = 1.8f;
     public float horizontalLimit = 2.6f;
+    public float firingSpeed = 3f;
+    public float firingCooldownDuration = 1f;
+    public GameObject missilePrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private bool fired = false;
+    private float CooldownTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
 
         //Horizontal Movement
         GetComponent<Rigidbody2D>().velocity = new Vector2(
@@ -27,10 +30,30 @@ public class Player : MonoBehaviour {
         {
             transform.position = new Vector3(horizontalLimit, transform.position.y, transform.position.z);
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        } if (transform.position.x < -horizontalLimit)
+        }
+        if (transform.position.x < -horizontalLimit)
         {
             transform.position = new Vector3(-horizontalLimit, transform.position.y, transform.position.z);
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
+        //Fire missiles
+
+        CooldownTimer -= Time.deltaTime;
+
+        if (CooldownTimer <= 0 && Input.GetAxis("Fire1") == 1f)
+        {
+            CooldownTimer = firingCooldownDuration;
+
+            GameObject missileInstance = Instantiate(missilePrefab);
+            missileInstance.transform.SetParent(transform.parent);
+            missileInstance.transform.position = transform.position;
+            missileInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(
+                0,
+                firingSpeed
+                );
+            Destroy(missileInstance, 2f);
+
         }
 
     }
